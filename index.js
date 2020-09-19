7d3e1070384335eff3182a34405d52c0a6815a0f
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-09-18 23:23:39 -0400

    Complete validate links

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var request = require("request");
+
 console.log("Hello, here is my first CLI tool")
 console.log(process.argv.length)
 
@@ -11,9 +13,30 @@ if (process.argv.length === 2) {
     var filename = process.argv[2];
     fs.readFile(filename, 'utf8', function (err, data) {
         if (err) throw err;
-        console.log("OK: " + filename);
-        console.log(data);
+        let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+        var links = data.match(regex);
+        for (var i = 0; i < links.length; i = i + 2) {
+            var link = links[i];
+            checkUrl(link);
+        }
     })
 }
 
-console.log("test")
+async function checkUrl(url) {
+    await urlValidate(url);
+}
+
+async function urlValidate(url) {
+    request({ method: 'HEAD', uri: url }, function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+            console.log("[PASS] [200] " + url);
+        } else if (!err && res.statusCode == 404) {
+            console.log("[DEAD] [404] " + url);
+        } else if (!err && res.statusCode == 400) {
+            console.log("[DEAD] [400] " + url);
+        } else {
+            console.log("[UNKOWN] " + url)
+        }
+    }
+    )
+}
