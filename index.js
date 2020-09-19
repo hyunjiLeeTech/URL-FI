6482d6b5eb83a1fb5ccc6329571147a6832a165b
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-09-18 21:56:35 -0400

    Complet check the num of args and print out usage

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
+
 console.log("Hello, here is my first CLI tool")
+console.log(process.argv.length)
+
+if (process.argv.length === 2) {
+    console.log("Usage: url-fi FILENAME")
+} else {
+    console.log("It has at least one argument")
+    console.log(process.argv[2])
+}
