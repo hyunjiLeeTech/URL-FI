a7623f7b64fa875cf0f035f62eadd3f16d07f24d
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-09-20 22:05:01 -0400

    Add an argument -v to show version

--- index.js
+++ index.js
@@ -2,30 +2,37 @@
 
 var request = require("request");
 var colors = require("colors");
+var fs = require("fs");
 
 if (process.argv.length === 2) {
     console.log("Usage: url-fi [FILENAME]")
     process.exit(1)
 } else {
-    var fs = require("fs");
-    var filename = process.argv[2];
-
-    fs.readFile(filename, 'utf8', function (err, data) {
-        if (err) throw err;
-        let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-        var links = data.match(regex);
-        for (var i = 0; i < links.length; i = i + 2) {
-            var link = links[i];
-            if (link.startsWith("https://")) {
-                checkUrl(link);
-                checkUrl(link.replace(/^https/, "http"));
-            } else {
-                checkUrl(link);
-                checkUrl(link.replace(/^http/, "https"));
+    for (var i = 2; i < process.argv.length; i++) {
+        var arg = process.argv[i];
+        if (arg.startsWith("--")) {
+            if (arg == "--v" || arg == "--version") {
+                console.log("Tool Name: url-fi")
+                console.log("Version: 0.1")
             }
+        } else {
+            fs.readFile(arg, 'utf8', function (err, data) {
+                if (err) throw err;
+                let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+                var links = data.match(regex);
+                for (var i = 0; i < links.length; i = i + 2) {
+                    var link = links[i];
+                    if (link.startsWith("https://")) {
+                        checkUrl(link);
+                        checkUrl(link.replace(/^https/, "http"));
+                    } else {
+                        checkUrl(link);
+                        checkUrl(link.replace(/^http/, "https"));
+                    }
+                }
+            })
         }
-    })
-
+    }
 }
 
 
