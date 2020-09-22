8d4d48aa25a3e088b12017ec3f21f8cccaf3cb91
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-09-21 20:24:00 -0400

    Change variables type

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
-var request = require("request");
-var colors = require("colors");
-var fs = require("fs");
+const request = require("request");
+const colors = require("colors");
+const fs = require("fs");
+const regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
 
 if (process.argv.length === 2) {
     console.log("Usage: url-fi [FILENAME]")
     process.exit(1)
 } else {
-    for (var i = 2; i < process.argv.length; i++) {
-        var arg = process.argv[i];
+    for (let i = 2; i < process.argv.length; i++) {
+        let arg = process.argv[i];
         if (arg.startsWith("--")) {
             if (arg == "--v" || arg == "--version") {
                 console.log("Tool Name: url-fi")
@@ -17,11 +18,13 @@ if (process.argv.length === 2) {
             }
         } else {
             fs.readFile(arg, 'utf8', function (err, data) {
-                if (err) throw err;
-                let regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-                var links = data.match(regex);
-                for (var i = 0; i < links.length; i = i + 2) {
-                    var link = links[i];
+                if (err) {
+                    console.log(colors.red("ERROR: " + err));
+                    process.exit(1);
+                }
+                let links = data.match(regex);
+                for (let i = 0; i < links.length; i = i + 2) {
+                    let link = links[i];
                     if (link.startsWith("https://")) {
                         checkUrl(link);
                         checkUrl(link.replace(/^https/, "http"));
