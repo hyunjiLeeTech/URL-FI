3fc21c6c6436cad150a16be8881a6092c4c5f63d
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-09-21 21:23:37 -0400

    Add comment

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const colors = require("colors");
 const fs = require("fs");
 const regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
 
+// If the user doesn't enter any arguments/filenames, it exits the process
 if (process.argv.length === 2) {
     console.log("Usage: url-fi [FILENAME]")
     process.exit(1)
 }
 
+// If the user enters any arguments/filenames, starts process.
+// --version or -v: prints tool name & version
+// filename: checks broken links
 for (let i = 2; i < process.argv.length; i++) {
     let arg = process.argv[i];
     if (arg.startsWith("--")) {
@@ -38,6 +42,10 @@ for (let i = 2; i < process.argv.length; i++) {
     }
 }
 
+// Checks the link is broken or not
+// - status code 200: good
+// - status code 400, 404: broken
+// - otherwise: unknown
 function checkUrl(url) {
     request({ method: 'HEAD', uri: url, timeout: 1500 }, function (err, res, body) {
         if (err) {
